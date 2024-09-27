a353acb99911977e6868285f5c3e0cf3446c6e4c
Tim Wagner <tim.wagner@check24.de>
2024-09-27 16:16:41 +0200

    created a basic app

---
+++ app/src/main/java/com/example/greetingapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.greetingapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.greetingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding?.apply {
+            submitButtonView.setOnClickListener {
+                resultTextView.text = "Hallo Welt"
+            }
+        }
+
+        enableEdgeToEdge()
+        setContentView(binding?.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
